05b81475a0edc79548faf314e1d522a0956ffc62
jack-xiaobaicai <jack-xiaobaicai@users.noreply.github.com>
2018-08-15 11:39:06 +0800

    Update cellphonealarm.js

--- cellphonealarm.js
+++ cellphonealarm.js
@@ -4,8 +4,8 @@
 "data":
 [
   {"onemonth":"3.88","threemonth":"6.88","oneyear":"18.88","flavor":"xiaomi"},
-  {"onemonth":"4.88","threemonth":"8.88","oneyear":"24.88","flavor":"huawei"},
-  {"onemonth":"5.88","threemonth":"10.88","oneyear":"30.88","flavor":"vivo"},
-  {"onemonth":"6.88","threemonth":"12.88","oneyear":"36.88","flavor":"oppo"}
+  {"onemonth":"2.99","threemonth":"5.99","oneyear":"25.99","flavor":"huawei"},
+  {"onemonth":"3.99","threemonth":"8.99","oneyear":"36.88","flavor":"vivo"},
+  {"onemonth":"1.99","threemonth":"3.99","oneyear":"18.88","flavor":"oppo"}
 ]
 }
