a8d01903d9ef377c2271e59ea025f073849f256b
jack-xiaobaicai <jack-xiaobaicai@users.noreply.github.com>
2018-10-08 14:25:40 +0800

    Update JiePaiQiJS.js

--- AdSwitchJS/JiePaiQiJS.js
+++ AdSwitchJS/JiePaiQiJS.js
@@ -1,82 +1,74 @@
-{\rtf1\ansi\ansicpg936\cocoartf1561\cocoasubrtf400
-{\fonttbl\f0\fnil\fcharset0 HelveticaNeue;}
-{\colortbl;\red255\green255\blue255;\red53\green53\blue53;}
-{\*\expandedcolortbl;;\cssrgb\c27059\c27059\c27059;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab560
-\pard\pardeftab560\slleading20\partightenfactor0
-
-\f0\fs24 \cf2 \{\
-  "status": true,\
-  "code": 200,\
-  "data": \{\
-    "vivo": \{\
-      "shoujiemain": \{\
-        "banner": \{\
-          "isshowbanner": true,\
-          "zidingyibanner": true,\
-          "zidngyibannnerimageurl": "www.baidu.com"\
-        \},\
-        "splash": \{\
-          "isshowsplash": true,\
-          "zidingyisplash": true,\
-          "zidngyisplashimageurl": "www.baidu.com"\
-        \},\
-        "tuia": \{\
-          "isshowtuia": true,\
-          "zidingyituia": true,\
-          "zidngyituiaimageurl": "www.baidu.com"\
-        \},\
-        "quanping": \{\
-          "isshowquanping": true,\
-          "zidingyiquanping": true,\
-          "zidngyiquanpingimageurl": "www.baidu.com"\
-        \}\
-      \},\
-      "edit": \{\
-        "banner": \{\
-          "isshowbanner": true,\
-          "zidingyibanner": true,\
-          "zidngyibannnerimageurl": "www.baidu.com"\
-        \},\
-        "splash": \{\
-          "isshowsplash": true,\
-          "zidingyisplash": true,\
-          "zidngyisplashimageurl": "www.baidu.com"\
-        \},\
-        "tuia": \{\
-          "isshowtuia": true,\
-          "zidingyituia": true,\
-          "zidngyituiaimageurl": "www.baidu.com"\
-        \},\
-        "quanping": \{\
-          "isshowquanping": true,\
-          "zidingyiquanping": true,\
-          "zidngyiquanpingimageurl": "www.baidu.com"\
-        \}\
-      \},\
-      "save": \{\
-        "banner": \{\
-          "isshowbanner": true,\
-          "zidingyibanner": true,\
-          "zidngyibannnerimageurl": "www.baidu.com"\
-        \},\
-        "splash": \{\
-          "isshowsplash": true,\
-          "zidingyisplash": true,\
-          "zidngyisplashimageurl": "www.baidu.com"\
-        \},\
-        "tuia": \{\
-          "isshowtuia": true,\
-          "zidingyituia": true,\
-          "zidngyituiaimageurl": "www.baidu.com"\
-        \},\
-        "quanping": \{\
-          "isshowquanping": true,\
-          "zidingyiquanping": true,\
-          "zidngyiquanpingimageurl": "www.baidu.com"\
-        \}\
-      \}\
-    \}\
-  \}\
-\}}
+{
+  "status": true,
+  "code": 200,
+  "data": {
+    "vivo": {
+      "shoujiemain": {
+        "banner": {
+          "isshowbanner": true,
+          "zidingyibanner": true,
+          "zidngyibannnerimageurl": "www.baidu.com"
+        },
+        "splash": {
+          "isshowsplash": true,
+          "zidingyisplash": true,
+          "zidngyisplashimageurl": "www.baidu.com"
+        },
+        "tuia": {
+          "isshowtuia": true,
+          "zidingyituia": true,
+          "zidngyituiaimageurl": "www.baidu.com"
+        },
+        "quanping": {
+          "isshowquanping": true,
+          "zidingyiquanping": true,
+          "zidngyiquanpingimageurl": "www.baidu.com"
+        }
+      },
+      "edit": {
+        "banner": {
+          "isshowbanner": true,
+          "zidingyibanner": true,
+          "zidngyibannnerimageurl": "www.baidu.com"
+        },
+        "splash": {
+          "isshowsplash": true,
+          "zidingyisplash": true,
+          "zidngyisplashimageurl": "www.baidu.com"
+        },
+        "tuia": {
+          "isshowtuia": true,
+          "zidingyituia": true,
+          "zidngyituiaimageurl": "www.baidu.com"
+        },
+        "quanping": {
+          "isshowquanping": true,
+          "zidingyiquanping": true,
+          "zidngyiquanpingimageurl": "www.baidu.com"
+        }
+      },
+      "save": {
+        "banner": {
+          "isshowbanner": true,
+          "zidingyibanner": true,
+          "zidngyibannnerimageurl": "www.baidu.com"
+        },
+        "splash": {
+          "isshowsplash": true,
+          "zidingyisplash": true,
+          "zidngyisplashimageurl": "www.baidu.com"
+        },
+        "tuia": {
+          "isshowtuia": true,
+          "zidingyituia": true,
+          "zidngyituiaimageurl": "www.baidu.com"
+        },
+        "quanping": {
+          "isshowquanping": true,
+          "zidingyiquanping": true,
+          "zidngyiquanpingimageurl": "www.baidu.com"
+        }
+      }
+    }
+  }
+}
