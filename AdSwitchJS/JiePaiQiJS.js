6c4a162a40de6c562197a93cc597d6f7c2d5b022
jack-xiaobaicai <jack-xiaobaicai@users.noreply.github.com>
2018-10-09 18:41:05 +0800

    Update JiePaiQiJS.js

--- AdSwitchJS/JiePaiQiJS.js
+++ AdSwitchJS/JiePaiQiJS.js
@@ -1,38 +1,92 @@
 {
-  "status": true,
-  "code": 200,
-  "data": {
-    "xiaomi": {
-      "isShowSplashAd": true,
-      "isShowHomeBanner": true,
-      "isShowHomeTuia": true,
-      "isShowSettingBanner": true,
-      "isShowJiePaiBanner": true,
-      "isShowYinSeBanner": true
-    },
-    "vivo": {
-      "isShowSplashAd": true,
-      "isShowHomeBanner": true,
-      "isShowHomeTuia": true,
-      "isShowSettingBanner": true,
-      "isShowJiePaiBanner": true,
-      "isShowYinSeBanner": true
-    },
-    "oppo": {
-      "isShowSplashAd": true,
-      "isShowHomeBanner": true,
-      "isShowHomeTuia": true,
-      "isShowSettingBanner": true,
-      "isShowJiePaiBanner": true,
-      "isShowYinSeBanner": true
-    },
-    "huawei": {
-      "isShowSplashAd": true,
-      "isShowHomeBanner": false,
-      "isShowHomeTuia": true,
-      "isShowSettingBanner": false,
-      "isShowJiePaiBanner": false,
-      "isShowYinSeBanner": false
+    "status": true,
+    "code": 200,
+    "data": {
+        "xiaomi": {
+            "isShowTenXunSplashAd": true,
+            "isShowHomeTenXunBanner": true,
+            "HomeFloatTuia": {
+                "isShowFloatTuia": true,
+                "refreshXiaomiTuia": {
+                    "isRefresh": true,
+                    "frequency": 20
+                },
+                "showFrequency": 20,
+                "tuiaImageUrl": [
+                    "www.baidu.com",
+                    "www.baidu.com"
+                ]
+            },
+            "isShowSettingTenXunBanner": true,
+            "isShowJiePaiTenXunBanner": true,
+            "isShowYinSeTenXunBanner": true
+        },
+        "vivo": {
+            "isShowTenXunSplashAd": true,
+            "isShowHomeTenXunBanner": true,
+            "HomeFloatTuia": {
+                "isShowFloatTuia": true,
+                "refreshVivoTuia": {
+                    "isRefresh": true,
+                    "frequency": 20
+                },
+                "showFrequency": 20,
+                "tuiaImageUrl": [
+                    "www.baidu.com",
+                    "www.baidu.com"
+                ]
+            },
+            "isShowSettingTenXunBanner": true,
+            "isShowJiePaiTenXunBanner": true,
+            "isShowYinSeTenXunBanner": true
+        },
+        "oppo": {
+            "isShowTenXunSplashAd": true,
+            "isShowHomeTenXunBanner": true,
+            "HomeFloatTuia": {
+                "isShowFloatTuia": true,
+                "refreshOppoTuia": {
+                    "isRefresh": true,
+                    "frequency": 20
+                },
+                "showFrequency": 20,
+                "tuiaImageUrl": [
+                    "www.baidu.com",
+                    "www.baidu.com"
+                ]
+            },
+            "isShowSettingTenXunBanner": true,
+            "isShowJiePaiTenXunBanner": true,
+            "isShowYinSeTenXunBanner": true
+        },
+        "huawei": {
+            "TuiaSplashAd": {
+                "isShowTuiaSplashAd": true,
+                "refreshSplash": {
+                    "isRefresh": true,
+                    "frequency": 20
+                },
+                "splashImageUrl": [
+                    "www.baidu.com",
+                    "www.baidu.com"
+                ]
+            },
+            "isShowHomeTenXunBanner": false,
+            "HomeFloatTuia": {
+                "isShowFloatTuia": true,
+                "refreshHuaweiTuia": {
+                    "isRefresh": true,
+                    "frequency": 20
+                },
+                "showFrequency": 20,
+                "tuiaImageUrl": [
+                    "www.baidu.com",
+                    "www.baidu.com"
+                ]
+            },
+            "isShowSettingTenXunBanner": false,
+            "isShowJiePaiTenXunBanner": false,
+            "isShowYinSeTenXunBanner": false
+        }
     }
-  }
 }
