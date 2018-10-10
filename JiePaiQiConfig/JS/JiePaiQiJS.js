3a59387ccfea645ad0f48b22d652f2bc44b3932a
jack-xiaobaicai <jack-xiaobaicai@users.noreply.github.com>
2018-10-10 11:30:22 +0800

    Add files via upload

---
+++ JiePaiQiConfig/JS/JiePaiQiJS.js
@@ -0,0 +1,239 @@
+{\rtf1\ansi\ansicpg936\cocoartf1561\cocoasubrtf400
+{\fonttbl\f0\fnil\fcharset0 HelveticaNeue;\f1\fnil\fcharset134 .PingFangSC-Regular;}
+{\colortbl;\red255\green255\blue255;\red53\green53\blue53;}
+{\*\expandedcolortbl;;\cssrgb\c27059\c27059\c27059;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab560
+\pard\pardeftab560\slleading20\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf2 \{\
+    "status": true
+\f1 ,
+\f0 \
+    "code": 200
+\f1 ,
+\f0 \
+    "data": 
+\f1 [
+\f0 \
+        \{\
+            "falvor": "xiaomi"
+\f1 ,
+\f0 \
+            "isShowTenXunSplashAd": true
+\f1 ,
+\f0 \
+            "isShowHomeTenXunBanner": true
+\f1 ,
+\f0 \
+            "isShowFloatTuia": true
+\f1 ,
+\f0 \
+            "isRefresh": true
+\f1 ,
+\f0 \
+            "frequency": 20
+\f1 ,
+\f0 \
+            "showFrequency": 20
+\f1 ,
+\f0 \
+            "isShowSettingTenXunBanner": true
+\f1 ,
+\f0 \
+            "isShowJiePaiTenXunBanner": true
+\f1 ,
+\f0 \
+            "isShowYinSeTenXunBanner": true
+\f1 ,
+\f0 \
+            "tuiaImageUrl": 
+\f1 [
+\f0 \
+                "https://yun.tuitiger.com/mami-media/img/mzzwmvzedi.gif"
+\f1 ,
+\f0 \
+                "https://yun.tuitiger.com/mami-media/img/onbi9xg7uc.gif"
+\f1 ,
+\f0 \
+                "https://yun.tuitiger.com/mami-media/img/vgpvrxxgmz.gif"
+\f1 ,
+\f0 \
+                "https://yun.tuitiger.com/mami-media/img/hxh9g2m5hk.png"\
+            
+\f1 ]
+\f0 \
+        \}
+\f1 ,
+\f0 \
+        \{\
+            "falvor": "vivo"
+\f1 ,
+\f0 \
+            "isShowTenXunSplashAd": true
+\f1 ,
+\f0 \
+            "isShowHomeTenXunBanner": true
+\f1 ,
+\f0 \
+            "isShowFloatTuia": true
+\f1 ,
+\f0 \
+            "isRefresh": true
+\f1 ,
+\f0 \
+            "frequency": 20
+\f1 ,
+\f0 \
+            "showFrequency": 20
+\f1 ,
+\f0 \
+            "isShowSettingTenXunBanner": true
+\f1 ,
+\f0 \
+            "isShowJiePaiTenXunBanner": true
+\f1 ,
+\f0 \
+            "isShowYinSeTenXunBanner": true
+\f1 ,
+\f0 \
+            "tuiaImageUrl": 
+\f1 [
+\f0 \
+                "https://yun.tuitiger.com/mami-media/img/mzzwmvzedi.gif"
+\f1 ,
+\f0 \
+                "https://yun.tuitiger.com/mami-media/img/onbi9xg7uc.gif"
+\f1 ,
+\f0 \
+                "https://yun.tuitiger.com/mami-media/img/vgpvrxxgmz.gif"
+\f1 ,
+\f0 \
+                "https://yun.tuitiger.com/mami-media/img/hxh9g2m5hk.png"\
+            
+\f1 ]
+\f0 \
+        \}
+\f1 ,
+\f0 \
+        \{\
+            "falvor": "oppo"
+\f1 ,
+\f0 \
+            "isShowTenXunSplashAd": true
+\f1 ,
+\f0 \
+            "isShowHomeTenXunBanner": true
+\f1 ,
+\f0 \
+            "isShowFloatTuia": true
+\f1 ,
+\f0 \
+            "isRefresh": true
+\f1 ,
+\f0 \
+            "frequency": 20
+\f1 ,
+\f0 \
+            "showFrequency": 20
+\f1 ,
+\f0 \
+            "isShowSettingTenXunBanner": true
+\f1 ,
+\f0 \
+            "isShowJiePaiTenXunBanner": true
+\f1 ,
+\f0 \
+            "isShowYinSeTenXunBanner": true
+\f1 ,
+\f0 \
+            "tuiaImageUrl": 
+\f1 [
+\f0 \
+                "https://yun.tuitiger.com/mami-media/img/mzzwmvzedi.gif"
+\f1 ,
+\f0 \
+                "https://yun.tuitiger.com/mami-media/img/onbi9xg7uc.gif"
+\f1 ,
+\f0 \
+                "https://yun.tuitiger.com/mami-media/img/vgpvrxxgmz.gif"
+\f1 ,
+\f0 \
+                "https://yun.tuitiger.com/mami-media/img/hxh9g2m5hk.png"\
+            
+\f1 ]
+\f0 \
+        \}
+\f1 ,
+\f0 \
+        \{\
+            "falvor": "huawei"
+\f1 ,
+\f0 \
+            "isShowTuiaSplashAd": true
+\f1 ,
+\f0 \
+            "isShowHomeTenXunBanner": false
+\f1 ,
+\f0 \
+            "isShowFloatTuia": true
+\f1 ,
+\f0 \
+            "isShowSettingTenXunBanner": false
+\f1 ,
+\f0 \
+            "isShowJiePaiTenXunBanner": false
+\f1 ,
+\f0 \
+            "isShowYinSeTenXunBanner": false
+\f1 ,
+\f0 \
+            "showSplashFrequency": 20
+\f1 ,
+\f0 \
+            "isSplashRefresh": true
+\f1 ,
+\f0 \
+            "Splashfrequency": 20
+\f1 ,
+\f0 \
+            "showTuiaFrequency": 20
+\f1 ,
+\f0 \
+            "isTuiaRefresh": true
+\f1 ,
+\f0 \
+            "Tuiafrequency": 20
+\f1 ,
+\f0 \
+            "tuiaImageUrl": 
+\f1 [
+\f0 \
+                "https://yun.tuitiger.com/mami-media/img/mzzwmvzedi.gif"
+\f1 ,
+\f0 \
+                "https://yun.tuitiger.com/mami-media/img/onbi9xg7uc.gif"
+\f1 ,
+\f0 \
+                "https://yun.tuitiger.com/mami-media/img/vgpvrxxgmz.gif"
+\f1 ,
+\f0 \
+                "https://yun.tuitiger.com/mami-media/img/hxh9g2m5hk.png"\
+            
+\f1 ],
+\f0 \
+            "splashImageUrl": 
+\f1 [
+\f0 \
+                "www.baidu.com"
+\f1 ,
+\f0 \
+                "www.baidu.com"\
+            
+\f1 ]
+\f0 \
+        \}\
+    
+\f1 ]
+\f0 \
+\}}
