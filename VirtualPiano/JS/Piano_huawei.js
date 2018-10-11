2b7d433741789429e83f97cc8ecb0c983a855ba4
jack-xiaobaicai <jack-xiaobaicai@users.noreply.github.com>
2018-10-11 15:59:12 +0800

    Add files via upload

---
+++ VirtualPiano/JS/Piano_huawei.js
@@ -0,0 +1,34 @@
+{\rtf1\ansi\ansicpg936\cocoartf1561\cocoasubrtf400
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 \{\
+	"status": true,\
+	"code": 200,\
+	"data": [\{\
+		"falvor": "huawei",\
+		"isShowAdHintDialog": false,\
+		"isShowTuiaSplashAd": true,\
+		"isShowHomeTenXunBanner": false,\
+		"isShowMianFloatTuia": true,\
+		"showSplashFrequency": 0,\
+		"isSplashRefresh": true,\
+		"showTuiaFrequency": 0,\
+		"isRefresh": true,\
+		"tuiaImageUrl": [\
+			"https://raw.githubusercontent.com/jack-xiaobaicai/logo/master/JiePaiQiConfig/customTuiaPic/customTuia1-1.gif",\
+			"https://raw.githubusercontent.com/jack-xiaobaicai/logo/master/JiePaiQiConfig/customTuiaPic/customTuia1-2.gif",\
+			"https://raw.githubusercontent.com/jack-xiaobaicai/logo/master/JiePaiQiConfig/customTuiaPic/customTuia1-3.gif"\
+		],\
+		"splashImageUrl": [\
+			"http://yun.tuitiger.com/mami-media/img/nqpm022m9a.jpg",\
+			"http://yun.tuitiger.com/mami-media/img/h8p3eo835n.jpg",\
+			"http://yun.tuitiger.com/mami-media/img/grv4kmu0ax.jpg",\
+			"http://yun.tuitiger.com/mami-media/img/kmqollmkm6.png",\
+			"http://yun.tuitiger.com/mami-media/img/ba1ybxgdf7.jpg"\
+		]\
+	\}]\
+\}}
---
+++ VirtualPiano/JS/Piano_vivo.js
@@ -0,0 +1,32 @@
+{\rtf1\ansi\ansicpg936\cocoartf1561\cocoasubrtf400
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 \{\
+	"status": true,\
+	"code": 200,\
+	"data": [\{\
+		"falvor": "vivo",\
+		"isShowAdHintDialog": true,\
+		"isShowTenXunSplashAd": true,\
+		"isShowHomeTenXunBanner": true,\
+		"isShowMianFloatTuia": true,\
+		"isRefresh": true,\
+		"showFrequency": 0,\
+		"isShowMianTenXunBanner": true,\
+		"isShowGoldnadequateiTenXunBanner": true,\
+		"isShowPianoScoreTenXunBanner": true,\
+		"isShowPlayTenXunBanner": true,\
+		"isShowPractiseTenXunBanner": true,\
+		"isShowRecordingListTenXunBanner": true,\
+		"isShowVipDialogTenXunBanner": true,\
+		"tuiaImageUrl": [\
+			"https://raw.githubusercontent.com/jack-xiaobaicai/logo/master/JiePaiQiConfig/customTuiaPic/customTuia1-1.gif",\
+			"https://raw.githubusercontent.com/jack-xiaobaicai/logo/master/JiePaiQiConfig/customTuiaPic/customTuia1-2.gif",\
+			"https://raw.githubusercontent.com/jack-xiaobaicai/logo/master/JiePaiQiConfig/customTuiaPic/customTuia1-3.gif"\
+		]\
+	\}]\
+\}}
