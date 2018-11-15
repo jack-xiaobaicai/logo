b6a2d48a828c874a351054e4d75e12dc5d10eacb
jack-xiaobaicai <jack-xiaobaicai@users.noreply.github.com>
2018-11-15 14:56:11 +0800

    Update JiePaiQiJS.js

--- JiePaiQiConfig/JS/JiePaiQiJS.js
+++ JiePaiQiConfig/JS/JiePaiQiJS.js
@@ -74,6 +74,22 @@
                 "http://yun.tuitiger.com/mami-media/img/kmqollmkm6.png",
                 "http://yun.tuitiger.com/mami-media/img/ba1ybxgdf7.jpg"
             ]
+        },
+         {
+            "falvor": "samsung",
+            "isShowTenXunSplashAd": true,
+            "isShowHomeTenXunBanner": false,
+            "isShowFloatTuia": true,
+            "isRefresh": true,
+            "showFrequency": 0,
+            "isShowSettingTenXunBanner": false,
+            "isShowJiePaiTenXunBanner": false,
+            "isShowYinSeTenXunBanner": false,
+            "tuiaImageUrl": [
+                "https://raw.githubusercontent.com/jack-xiaobaicai/logo/master/JiePaiQiConfig/customTuiaPic/customTuia1-1.gif",
+                "https://raw.githubusercontent.com/jack-xiaobaicai/logo/master/JiePaiQiConfig/customTuiaPic/customTuia1-2.gif",
+                "https://raw.githubusercontent.com/jack-xiaobaicai/logo/master/JiePaiQiConfig/customTuiaPic/customTuia1-3.gif"
+            ]
         }
     ]
 }
